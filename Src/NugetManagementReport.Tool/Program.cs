48febddea46eafe9214920437395d897064a0d61
Chinonso Asuzu <147524533+nonsowd@users.noreply.github.com>
2023-11-30 20:39:40 +0000

    #5 Initial project setup

---
+++ Src/NugetManagementReport.Tool/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NugetManagementReport.Tool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
