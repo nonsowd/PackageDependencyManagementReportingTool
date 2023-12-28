bab0643a0102b435a8b4a322b17f5356859e7597
Chinonso Asuzu <147524533+nonsowd@users.noreply.github.com>
2023-12-28 20:21:11 +0000

    #5 RunCommand Test Fixed

--- Src/NugetManagementReport.Test/UI/RunCommand_UnitTests.cs
+++ Src/NugetManagementReport.Test/UI/RunCommand_UnitTests.cs
@@ -1,23 +1,24 @@
 using NugetManagementReport.Application;
-using NugetManagementReport.Tool;
+using NugetManagementReport.Tool.Commands;
 
 namespace NugetManagementReport.Test.UI;
 
-//public class RunCommand_Excute
-//{
-//    [Fact]
-//    [Trait("Category", "UnitTest")]
-//    public void Should_DelegateTo_RunCommandHandler_Handle()
-//    {
-//        // Arrange
-//        var mockRunCommandHandler = new Mock<RunCommandHandler>();
-//        var runCommand = new RunCommand(mockRunCommandHandler.Object);
-//        var filePath = string.Empty;
-        
-//        // Act
-//        runCommand.Execute(filePath);
+public class RunCommand_Excute
+{
+    [Fact]
+    [Trait("Category", "UnitTest")]
+    public void Should_DelegateTo_RunCommandHandler_Handle()
+    {
+        // Arrange
+        var mockRunCommandHandler = new Mock<IRunCommandHandler>();
+        var runCommand = new RunCommand(mockRunCommandHandler.Object);
+        var filePath = string.Empty;
+        var settings = new RunCommand.Settings { FilePath = filePath };
 
-//        // Assert 
-//        mockRunCommandHandler.Verify(x => x.Handle(filePath));
-//    }
-//}
+        // Act
+        runCommand.Execute(null!, settings);
+
+        // Assert 
+        mockRunCommandHandler.Verify(x => x.Handle(filePath));
+    }
+}
